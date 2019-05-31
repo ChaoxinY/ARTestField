5da4d5edd16d15ae58958cb93f49ecb34fb63f4f
Chaoxin <chaoxin_yang@hotmail.com>
2019-05-31 13:37:10 +0200

    Finished https://codelabs.developers.google.com/codelabs/arcore-intro/#12 tutorial to step 12.

---
+++ ARTestField/Assets/Scripts/SceneController.cs
@@ -0,0 +1,77 @@
+﻿using GoogleARCore;
+using UnityEngine;
+
+public class SceneController : MonoBehaviour
+{
+    public Camera firstPersonCamera;
+    public ScoreBoardController scoreboard;
+    public SnakeController snakeController;
+
+    void Start()
+    {
+        QuitOnConnectionErrors();
+    }
+
+    void Update()
+    {
+        // The session status must be Tracking in order to access the Frame.
+        if (Session.Status != SessionStatus.Tracking)
+        {
+            int lostTrackingSleepTimeout = 15;
+            Screen.sleepTimeout = lostTrackingSleepTimeout;
+            return;
+        }
+        Screen.sleepTimeout = SleepTimeout.NeverSleep;
+        ProcessTouches();
+    }
+
+    void QuitOnConnectionErrors()
+    {
+        //Is the permission to use the camera granted?
+        if (Session.Status == SessionStatus.ErrorPermissionNotGranted)
+        {
+            StartCoroutine(CodelabUtils.ToastAndExit(
+                "Camera permission is needed to run this application.", 5));
+        }
+        else if (Session.Status.IsError())
+        {
+            // This covers a variety of errors.  See reference for details
+            // https://developers.google.com/ar/reference/unity/namespace/GoogleARCore
+            StartCoroutine(CodelabUtils.ToastAndExit(
+                "ARCore encountered a problem connecting. Please restart the app.", 5));
+        }     
+    }
+
+
+    /// <summary>
+    /// To process the touches, we get a single touch and raycast it using the ARCore session to check if the user tapped on a plane.
+    /// If so, we'll use that one to display the rest of the objects.
+    /// </summary>
+    void ProcessTouches()
+    {
+        Touch touch;
+        if (Input.touchCount != 1 ||
+            (touch = Input.GetTouch(0)).phase != TouchPhase.Began)
+        {
+            return;
+        }
+
+        TrackableHit hit;
+        TrackableHitFlags raycastFilter =
+            TrackableHitFlags.PlaneWithinBounds |
+            TrackableHitFlags.PlaneWithinPolygon;
+
+        if (Frame.Raycast(touch.position.x, touch.position.y, raycastFilter, out hit))
+        {
+            SetSelectedPlane(hit.Trackable as DetectedPlane);
+        }
+    }
+
+    void SetSelectedPlane(DetectedPlane selectedPlane)
+    {
+        scoreboard.SetSelectedPlane(selectedPlane);
+        snakeController.SetPlane(selectedPlane);
+        Debug.Log("Selected plane centered at " + selectedPlane.CenterPose.position);
+    }
+
+}
